a5cf9c05e0492a35d0d900ec2e61b094df51c847
Long Su <z5452313@ad.unsw.edu.au>
2023-11-06 12:47:27 +0800

    submit change

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import PageList from './components/pageList';
+
+function App () {
+  return (
+    <>
+      <Router>
+        <PageList />
+      </Router>
+    </>
+  );
+}
+
+export default App;
