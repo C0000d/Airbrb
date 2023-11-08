59e4eff58aa02e305980b18a3843e4cad421ea43
Desheng Liu <liudesheng1994@gmail.com>
2023-11-08 22:59:21 +1100

    progress

---
+++ frontend/src/components/fileToDataURL.tsx
@@ -0,0 +1,16 @@
+const FileToDataUrl = async (file: File):Promise<string> => {
+  const validFileTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+  const valid = validFileTypes.find((type) => type === file.type);
+  // Bad data, let's walk away.
+  if (!valid) {
+    throw Error('provided file is not a png, jpg or jpeg image.');
+  }
+  const reader = new FileReader();
+  return new Promise<string>((resolve, reject) => {
+    reader.onerror = () => reject(reader.error);
+    reader.onload = () => resolve(reader.result as string);
+    reader.readAsDataURL(file);
+  });
+}
+
+export default FileToDataUrl;
